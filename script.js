07281ffc8c314ea4638b708e7a97edb81afac941
KostetskyS <kostetsky1337@gmail.com>
2023-03-01 23:26:33 +0200

    1st

---
+++ script.js
@@ -0,0 +1,29 @@
+fetch ('https://api.openweathermap.org/data/2.5/weather?id=703448&appid=5f2a3794c58f13226552980c675ae34d') 
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        document.querySelector('.city').textContent += data.name;
+        document.querySelector('.temperature p').innerHTML += Math.round(data.main.temp - 273) + '&deg';
+        document.querySelector('.weather').textContent += data.weather[0]['description'];
+        document.querySelector('.wind').textContent += data.wind['speed'] + ' ' + 'm/s';
+        document.querySelector('.pict p').innerHTML = `<img src= "https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png"> `;
+        let widgetBackground = document.querySelector('.widget');
+        if (data.weather[0]['description'] == 'overcast clouds') {
+            widgetBackground.classList.add('foggy');
+            console.log('dsadsa')
+        }
+        // if (data.weather[0]['description'] == 'overcats clouds') {
+        //     widgetBackground.classList.add('foggy');
+        // }
+        // if (data.weather[0]['description'] == 'overcats clouds') {
+        //     widgetBackground.classList.add('sunny');
+        // }
+        // if (data.weather[0]['description'] == 'overcats clouds') {
+        //     widgetBackground.classList.add('storm');
+        // }
+        console.log(data);
+    })
+    .catch(function() {
+        alert('памилка')
+    })
